8f0f7dd7354ee2f4e589a64e7524b360da0be3e9
b̵̛̩̖̳̙̩̲̐͗̏͒̈̕0̵̟͖̰͈̝̄̈́̋̄͊̚͝b̶̧̠͇̹̪̲̼̪̄́̓̔ë̸̝͕̗͓́͂͌̐̚ͅr̵̨͍̰͈̬̭͑̑̇ <125749439+Zenderrrr@users.noreply.github.com>
2025-11-27 10:39:59 +0100

    changed the location of documentation files

--- obj/Debug/net9.0/Timetable-Project.AssemblyInfo.cs
+++ obj/Debug/net9.0/Timetable-Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Timetable-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9503d8a6fb2ac81498282162fe98c8c0ea521c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7593d337714450959c23f1fb56d2530485c5bd1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Timetable-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Timetable-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
